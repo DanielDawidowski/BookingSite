7d74e63cce4b8857d8add86d14786cea4b258a02
Daniel Dawidowski <dvds1987@gmail.com>
2019-11-03 12:02:57 +0100

    Fix Bug In Merge.js

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -14,10 +14,14 @@ const userLoader = new DataLoader(userIds => {
 
 const events = async eventIds => {
     try {
-        const events = await Event.find({_id: {$in: eventIds}})
+        const events = await Event.find({ _id: { $in: eventIds } })
+        events.sort((a,b) => {
+            return eventIds.indexOf(a._id.toString()) - eventIds.indexOf(b._id.toString())
+        })
+        console.log(events, eventIds)
         return events.map(event => {
             return transformEvent(event)
-        })
+        })  
     } catch (err) {
         throw err
     }
